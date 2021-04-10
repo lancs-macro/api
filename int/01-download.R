528752ba5182ab6e5407f0014cbb07c94acc7148
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2021-04-10 16:01:42 +0100

    add netlify.toml

--- int/01-download.R
+++ int/01-download.R
@@ -1,1 +1,9 @@
-library(nationwider)
+library(tidyverse)
+data <- ihpdr::ihpd_get("raw") %>% 
+  select(Date, country, rhpi) %>% 
+  pivot_wider(values_from = rhpi, names_from = country)
+
+
+
+jsonlite::write_json(mtcars, here::here("public", "index.json"))
+
