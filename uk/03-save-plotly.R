3809d7b4b80eebb8fd3a164f87a9de4caf436804
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2021-04-25 17:56:55 +0100

    remove git history and uploaded new small files

--- uk/03-save-plotly.R
+++ uk/03-save-plotly.R
@@ -6,14 +6,19 @@ for(i in 1:length(snames)) {
   htmlwidgets::saveWidget(
     widget = plotly::partial_bundle(aplt), 
     file = paste0("public/visualizations/", file_snames[i], ".html"),
-    selfcontained = TRUE,
-    libdir = "libs",
     selfcontained = FALSE,
+    libdir = "libs",
     title = snames[i]
   )
 }
 
-
-for(i in series_names(radf_rhpi)) {
-  aplt_pti[[i]] <- autoplot(radf_rhpi, mc_cv, select_series = i)
+for(i in 1:length(snames)) {
+  aplt <- plotly::ggplotly(autoplot(radf_pti, mc_cv, select_series = snames[i]))
+  htmlwidgets::saveWidget(
+    widget = plotly::partial_bundle(aplt), 
+    file = paste0("public/visualizations/", file_snames[i], ".html"),
+    selfcontained = FALSE,
+    libdir = "libs",
+    title = snames[i]
+  )
 }
