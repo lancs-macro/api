200876421c19e945aa5ef2b47a466e5b2d22870e
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2022-05-24 09:02:51 +0100

    fix version of psyivx data

--- int/01-download.R
+++ int/01-download.R
@@ -37,7 +37,7 @@ mc_cv <- radf_crit[[NROW(rhpi)]]
 
 # psyivx q1 ---------------------------------------------------------------
 
-datafile <- here::here("data-raw", "int", "psyivx-2021q3.xlsx")
+datafile <- here::here("data-raw", "int", "psyivx-2021q4.xlsx")
 nms0 <- gsub("\\.\\.", "\\.", names(readxl::read_excel(datafile)))[-1]
 sheets <- excel_sheets(datafile)
 suppressMessages({
