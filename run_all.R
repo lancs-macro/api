4b7fd402554ef7943a23bf052bc6c5e23811e2d4
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2024-01-15 09:14:58 +0200

    fix 2023q2 hopi data

--- run_all.R
+++ run_all.R
@@ -1,5 +1,11 @@
-# uk ----------------------------------------------------------------------
 
+pkgs <- c('exuber','here')
+remote_pkgs <- c('kvasilopoulos/ihdpr','kvasilopoulos/ivx','kvasilopoulos/nationwider')
+remotes::install_github('kvasilopoulos/ihdpr')
+remotes::install_github('kvasilopoulos/ivx')
+remotes::install_github('kvasilopoulos/nationwider')
+
+# uk ----------------------------------------------------------------------
 
 source("uk/02-write-json.R")
 source("uk/03-write-plotly.R")
